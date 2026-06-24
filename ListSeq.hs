249608218d95bce3a4d49d74ddda85bbeb591cc2
Franco Franzini <francofranzini12@gmail.com>
2026-06-24 16:36:48 -0300

    Correcciones

--- ListSeq.hs
+++ ListSeq.hs
@@ -1,37 +1,37 @@
 module ListSeq where
 
 import Seq
-
+import Par
 newtype ListSeq a = ListSeq [a]
 
 instance Seq ListSeq where
-    emptyS                             = []
-    singletonS x                       = [x]
+    emptyS                             = ListSeq []
+    singletonS x                       = ListSeq [x]
     lengthS (ListSeq s)                = length s
     nthS (ListSeq s) n                 = s !! n
-    tabulateS f (ListSeq s)            = ListSeq (map f [0..n-1])
+    tabulateS f n                      = ListSeq (map f [0..n-1])
     mapS f (ListSeq s)                 = ListSeq (map f s)
     filterS f (ListSeq s)              = ListSeq (filter f s)
     appendS (ListSeq s1) (ListSeq s2)  = ListSeq (s1 ++ s2)
     takeS (ListSeq s) n                = ListSeq (take n s)
     dropS (ListSeq s) n                = ListSeq (drop n s)
-    showtS (emptyS)                    = EMPTY
-    showtS (singletonS x)              = ELT x
+    showtS (ListSeq [])                = EMPTY
+    showtS (ListSeq [x])               = ELT x
     showtS (ListSeq s)                 = NODE (ListSeq (take mitad s))
                                               (ListSeq (drop mitad s))
-                                              where mitad = (length s)/2
-    showlS (emptyS)                    = NIL
-    showlS (singletonS x)              = CONS x NIL
-    showlS (ListSeq s)                 = CONS (take 1 s) (ListSeq (drop 1 s))
+                                              where mitad = (length s) `div` 2
+    showlS (ListSeq [])                = NIL
+    showlS (ListSeq (x:xs))            = CONS x (ListSeq xs)
 
-    joinS (emptyS)                     = ListSeq []
-    joinS (ListSeq s)                  = ListSeq (concatMap (\(ListSec xs) -> xs) s)
+    joinS (ListSeq [])                 = ListSeq []
+    joinS (ListSeq s)                  = ListSeq (concatMap (\(ListSeq xs) -> xs) s)
     
-    reduceS f b (emptyS)               = b
-    reduceS f b (singletonS x)         = x
-    reduceS f b (ListSeq s)            = f (reduce f b (take mitad s))
-                                           (reduce f b (drop mitad s)) 
-                where mitad = (length s) `div` 2
+    reduceS f b (ListSeq [])           = b
+    reduceS f b (ListSeq [x])          = x
+    reduceS f b (ListSeq s)            = 
+        let (izq, der) = (reduceS f b (ListSeq (take mitad s))) ||| (reduceS f b (ListSeq (drop mitad s)))
+            mitad = (length s) `div` 2
+        in f izq der
     scanS f b (ListSeq s)              = (ListSeq prefijos, total)
         where
             acum = scanl f b s
